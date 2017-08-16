0d9fe82f0921c8a56145b3460ecbcbbd1a6ca393
Allan Bian <83742368@qq.com>
2017-08-16 22:08:53 +0800

    update

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ var http = require('http');
 http.createServer(function(req,res){
     res.writeHead(200,{'Content-Type':'text/plain'});
     res.end('欢迎来到我的阿里云服务器!\n');
-}).listen(80,"0.0.0.0");
+}).listen(80,"http://139.224.117.182");
 
 console.log('NodeJS Server running at http://127.0.0.1:80');
