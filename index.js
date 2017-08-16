226a5c0b9c2ee680f0c91ff8cb9107434cffe147
Allan Bian <83742368@qq.com>
2017-08-16 23:12:34 +0800

    update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var http = require('http');
 
 http.createServer(function(req,res){
-    res.writeHead(200,{'Content-Type':'text/html;charset=utf-8'});
+    res.writeHead(200,{'Content-Type':'text/plain;charset=utf-8'});
     res.end('顾晓倩我好爱你哦!!!\n');
 }).listen(80,"0.0.0.0");
 
