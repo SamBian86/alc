50fe1e6b5883ff17a8c6cb2818b48124dedf2f98
bxdsam <83742368@qq.com>
2018-01-24 23:37:35 +0800

    express项目提交

--- index.js
+++
@@ -1,8 +0,0 @@
-var http = require('http');
-
-http.createServer(function(req,res){
-    res.writeHead(200,{'Content-Type':'text/html;charset=utf-8'});
-    res.end('顾晓倩我好爱你哦!!!\n<p style="width:100%;position:fixed; bottom:0; text-align:center;"><a href="http://www.miitbeian.gov.cn/">苏ICP备17050016号</a></p>');
-}).listen(80,"0.0.0.0");
-
-console.log('NodeJS Server running at http://127.0.0.1:80');
---
+++ routes/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { 
+  	msg: '顾晓倩我好爱你哦!!!',
+  	copyright : '苏ICP备17050016号',
+  	copyrightLink : 'http://www.miitbeian.gov.cn/'
+  });
+});
+
+module.exports = router;
