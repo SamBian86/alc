e60c68a79e87cd00e8f5bcc7f43eb8a36c9f5922
bxdsam <83742368@qq.com>
2018-07-14 14:05:21 +0800

    添加微信相关代码

--- app.js
+++ app.js
@@ -4,6 +4,13 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var wechat = require('wechat');
+var config = {
+  token: 'bxd5812127',
+  appid: 'wx09957e6f988b2bd6',
+  encodingAESKey: '',
+  checkSignature: true // 可选，默认为true。由于微信公众平台接口调试工具在明文模式下不发送签名，所以如要使用该测试工具，请将其设置为false
+};
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -21,9 +28,18 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(express.query());
 app.use('/', index);
 app.use('/users', users);
+
+app.use('/wechat', wechat(config, function (req, res, next) {
+  // 微信输入信息都在req.weixin上
+  var message = req.weixin;
+  if (message) {
+    // 回复屌丝(普通回复)
+    res.reply('haha');
+  }
+}));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -42,5 +58,8 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+
+
 
 module.exports = app;
