14ac6147b7a4461b0d75c3fa7fe5237e1ce295a9
bxdsam <83742368@qq.com>
2018-07-14 14:41:52 +0800

    提交key

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var wechat = require('wechat');
 var config = {
   token: 'bxd5812127',
   appid: 'wx09957e6f988b2bd6',
-  encodingAESKey: '',
+  encodingAESKey: '54GPqyBMPZp15LmXXIvQJZEwLivzV2nqreabWDgaAtc',
   checkSignature: true // 可选，默认为true。由于微信公众平台接口调试工具在明文模式下不发送签名，所以如要使用该测试工具，请将其设置为false
 };
 
