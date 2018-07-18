c81a5f1607e20a873144c5c31abf9f9195ea0852
卞晓冬 <83742368@qq.com>
2018-07-18 22:41:59 +0800

    just test

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { 
+  res.render('index', {
   	msg: '顾晓倩我好爱你哦!!!',
   	copyright : '苏ICP备17050016号',
   	copyrightLink : 'http://www.miitbeian.gov.cn/'
   });
 });
-
 
 
 module.exports = router;
