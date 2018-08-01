14761fdfb6b8fac5a8b233e83cccf88a46c38df0
bxdsam <83742368@qq.com>
2018-08-01 23:20:09 +0800

    添加eslint相关文件

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,13 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express')
+const router = express.Router()
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { 
-  	msg: '顾晓倩我好爱你哦!!!',
-  	copyright : '苏ICP备17050016号',
-  	copyrightLink : 'http://www.miitbeian.gov.cn/'
-  });
-});
+router.get('/', (req, res, next) => {
+  res.render('index', {
+    msg: '顾晓倩我好爱你哦!!!',
+    copyright: '苏ICP备17050016号',
+    copyrightLink: 'http://www.miitbeian.gov.cn/'
+  })
+})
 
-
-
-module.exports = router;
+module.exports = router
--- routes/wechat.js
+++ routes/wechat.js
@@ -1,22 +1,22 @@
-var wechat = require('wechat');
-var express = require('express');
-var router = express.Router();
-var config = {
+const wechat = require('wechat')
+const express = require('express')
+const router = express.Router()
+const config = {
   token: 'bxd5812127',
   appid: 'wx09957e6f988b2bd6',
   encodingAESKey: '54GPqyBMPZp15LmXXIvQJZEwLivzV2nqreabWDgaAtc',
   checkSignature: true // 可选，默认为true。由于微信公众平台接口调试工具在明文模式下不发送签名，所以如要使用该测试工具，请将其设置为false
-};
+}
 
-/* all listing. */
-router.all('/', wechat(config, function (req, res, next) {
-  // 微信输入信息都在req.weixin上
-  var message = req.weixin;
+// 微信输入信息都在req.weixin上
+router.all('/', wechat(config, (req, res, next) => {
+  const message = req.weixin
+
   if (message.Content !== '') {
-    res.reply('乱打咩字呀,仆街仔!~');
+    res.reply('乱打咩字呀,仆街仔!~')
   } else {
     res.reply('感谢您的关注')
   }
-}));
+}))
 
-module.exports = router;
+module.exports = router
