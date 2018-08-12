4b27f5921d10fdb79ce0e77fe04952a185d387ad
bxdsam <83742368@qq.com>
2018-08-12 13:58:26 +0800

    解决网络变慢的问题

--- public/javascripts/birthday.js
+++ public/javascripts/birthday.js
@@ -1,8 +1,8 @@
 /*
  * @Author: bxdsam
  * @Date:   2018-08-05 23:35:22
- * @Last Modified by:   xiaodong.bian
- * @Last Modified time: 2018-08-06 17:49:44
+ * @Last Modified by:   bxdsam
+ * @Last Modified time: 2018-08-12 13:55:55
  */
 (function(win) {
   var num = 0;
@@ -14,30 +14,35 @@
   var hatWhite = document.querySelector('.hat-white');
   var hatYellow = document.querySelector('.hat-yellow');
   var yaoqinghan = document.querySelector('.yaoqinghan');
+  var imgs = document.getElementsByTagName('img');
   var timer = null;
+  console.log('imgs.length:' + imgs.length)
   // var audio = document.getElementById('audio');
   // audio#audio(autoplay='autoplay' loop='true' hidden='true' controls='controls')
   //   source(src='/video/qingcunwuqu.mp3?v=12' type='audio/mpeg')
 
   win.onload = function(){
 
-    timer = window.setTimeout(function(){
-      animateStart();
-    },1000)
-
-    function animateStart(){
-      if (num === 9) {
-        animateFun();
-        window.clearTimeout(timer)
-      }else{
-        timer = window.setTimeout(function(){
-          animateStart();
-        },1000)
-      }
+    for(var i = 0; i < imgs.length; i++){
+      //(function (that) {
+        var _src = imgs[i].dataset['setImgSrc'];
+        imgs[i].src = _src
+      //})(i)
     }
 
   }
 
+
+  function animateStart(){
+    if (num === 9) {
+      animateFun();
+      window.clearTimeout(timer)
+    }else{
+      timer = window.setTimeout(function(){
+        animateStart();
+      },1000)
+    }
+  }
   function animateFun() {
 
     window.setTimeout(function() {
@@ -70,6 +75,12 @@
   function imgLoad(e) {
     num++;
     console.log(num)
+
+    if(num === 9){
+      timer = window.setTimeout(function(){
+        animateStart();
+      },1000)
+    }
   };
 
   win.imgLoad = imgLoad
