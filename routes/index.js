cc2648257eb8ddceb89edded044db22faf00f9a1
83742368@qq.com <83742368@qq.com>
2020-03-09 09:18:53 +0800

    工信部地址修改

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,20 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
 /* GET home page. */
-router.get('/', (req, res, next) => {
-  res.render('index', {
-    msg: '顾晓倩我好爱你哦!!!',
-    copyright: '苏ICP备17050016号',
-    copyrightLink: 'http://www.miitbeian.gov.cn/'
-  })
-})
+router.get("/", (req, res, next) => {
+  res.render("index", {
+    msg: "顾晓倩我好爱你哦!!!",
+    copyright: "苏ICP备17050016号",
+    copyrightLink: "http://www.beian.miit.gov.cn"
+  });
+});
 
 // f1ccb8
-router.get('/birthday', (req, res, next) => {
-  res.render('birthday', {
-    title: '卞梦玥小朋友百日宴邀请函'
-  })
-})
+router.get("/birthday", (req, res, next) => {
+  res.render("birthday", {
+    title: "卞梦玥小朋友百日宴邀请函"
+  });
+});
 
-module.exports = router
-
+module.exports = router;
